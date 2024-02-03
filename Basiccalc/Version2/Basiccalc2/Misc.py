4273fe3bb625239d8291e715e3df178cf86e99c2
maky5545 <46965117+maky5545@users.noreply.github.com>
2024-02-03 01:50:18 +0000

    Add files via upload
    updated the code in Main to include a bit more error handling, streamlined code and added a few more features. Added 2 new defs in misc.

--- Basiccalc/Version2/Basiccalc2/Misc.py
+++ Basiccalc/Version2/Basiccalc2/Misc.py
@@ -1,11 +1,19 @@
 import os
 from Checks import calcFileDirec, fileName
+import subprocess
+import time
 
 def fileLocation():
-    print("A calculation files with all of the equations and answers has"
-       " been created here: " + "\n" + calcFileDirec + fileName)
+    print("\nA calculation files with all of the equations and answers has"
+       " been created here:")
+    print("\033[92m" + f"{calcFileDirec}{fileName}" + "\033[0m")
 
+def openFileLocation():
+    subprocess.Popen(f'explorer "{calcFileDirec}"')
 
 # clears the console
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def consoleSleep2():
+    time.sleep(2)
