306a1839e56f6683f35e46de7d0e0f0dface2c84
Maky5545 <marcusbentley15@gmail.com>
2024-02-05 01:13:00 +0000

    Add fileLocation() and openFileLocation() functions

--- Basiccalc/Version2/Basiccalc2/Main.py
+++ Basiccalc/Version2/Basiccalc2/Main.py
@@ -1,16 +1,15 @@
 # Imports
 import re # Regular Expressions to split the text in the calculation
-from Checks import noDecimal # Check if the number has a decimal part
-from Checks import fileCreator # Check if directory and file exsists, if not, create them, then write the values to the files.
-from Misc import clear_console # Clears the console
-from Misc import fileLocation # Prints the location of the file
-from Operators import add_numbers # Addition
-from Operators import sub_numbers # Subtraction
-from Operators import div_numbers # Division
-from Operators import mul_numbers # Multiplication
+from Checks import noDecimal, fileCreator
+from Misc import clear_console, fileLocation, openFileLocation, consoleSleep2
+from Operators import add_numbers, sub_numbers, div_numbers, mul_numbers
 
 # Define General Variables
 mainLoop = True # Main program loop
+loopCount = 0 # Loop counter
+userInput = False # User input loop
+exitProg = False # Exit the program
+firstNumber, operation, secondNumber = 0, "", 0 # Variables for the user's calculation
 
 # Intro Strings
 print(str.center("Welcome to the calculator!", 50,))
@@ -18,13 +17,30 @@ print(str.center('Type "Exit" at any time to leave', 50,))
 
 # Main Program Loop
 while mainLoop == True:
-    userCalc = input("\n" + "Please input the calculation: ").rstrip("\n") # variable for the user's calculation
-    if userCalc == "Exit" or userCalc == "exit": # Exit the program if the user types "Exit"
-        mainLoop = False # just for good mesure
-        break # Exit the main program loop if the user types "Exit"
+    while userInput == False:                    
+        userCalc = input("\n" + "Please input the calculation: ").rstrip() # variable for the user's calculation
+        if userCalc.lower() == "exit":
+            exitProg = True
+            break
+        else:
+            try:
+                # variable: split the input into 3 parts: number1, operation, number2
+                firstNumber, operation, secondNumber = re.split(r'(\+|\-|\*|\/)', userCalc)
+                userInput = True # Exit the user input loop
+            except ValueError:
+                if operation != "+" or operation != "-" or operation != "*" or operation != "/":
+                    input("\033[1;31mERROR: Invalid input, press enter to try again.\033[0m")
+                    clear_console()
+                    userInput = False # Re-enter the user input loop
+                else:
+                    input("\033[1;31mVALUE ERROR: \033[0m" + "Too many or too few values, press enter to try again.")
+                    clear_console()
+                    userInput = False # Re-enter the user input loop
+                    
     
-    # variable: split the input into 3 parts: number1, operation, number2
-    firstNumber, operation, secondNumber = re.split(r'(\+|\-|\*|\/)', userCalc)
+    # Exit the loop if the user types "Exit or exit"
+    if exitProg == True:
+        break
 
     # Convert the strings to integers or floats depending on if they have a decimal part
     if noDecimal(float(firstNumber)) == True:
@@ -36,50 +52,42 @@ while mainLoop == True:
     elif noDecimal(float(secondNumber)) == False:
         secondNumber = float(secondNumber)
 
-    # Main Program Logic using if statements
-    if operation == "+": # Addition
+    if operation == "+":
         opAnswer = add_numbers(firstNumber, secondNumber)
-        if noDecimal(opAnswer) == True: # Check if the answer has a decimal part
-            
-            print("\n" + str(firstNumber) + operation + str(secondNumber) + " = " + str(int(opAnswer))) # Print the answer to console
-            fileCreator(firstNumber, secondNumber, operation, int(opAnswer)) # Write the answer to the file
-
-        elif noDecimal(opAnswer) == False:
-            print("\n" + str(firstNumber) + operation + str(secondNumber) + " = " + str(float(opAnswer))) # Print the answer to console
-            fileCreator(firstNumber, secondNumber, operation, float(opAnswer)) # Write the answer to the file
-    
     elif operation == "-":
         opAnswer = sub_numbers(firstNumber, secondNumber)
-        if noDecimal(opAnswer) == True: # Check if the answer has a decimal part
-            print("\n" + str(firstNumber) + operation + str(secondNumber) + " = " + str(int(opAnswer))) # Print the answer to console
-            fileCreator(firstNumber, secondNumber, operation, int(opAnswer)) # Write the answer to the file
-
-        elif noDecimal(opAnswer) == False:
-            print("\n" + str(firstNumber) + operation + str(secondNumber) + " = " + str(float(opAnswer))) # Print the answer to console
-            fileCreator(firstNumber, secondNumber, operation, float(opAnswer)) # Write the answer to the file
-
     elif operation == "/":
         opAnswer = div_numbers(firstNumber, secondNumber)
-        if noDecimal(opAnswer) == True: # Check if the answer has a decimal part
-            print("\n" + str(firstNumber) + operation + str(secondNumber) + " = " + str(int(opAnswer))) # Print the answer to console
-            fileCreator(firstNumber, secondNumber, operation, int(opAnswer)) # Write the answer to the file
-
-        elif noDecimal(opAnswer) == False:
-            print("\n" + str(firstNumber) + operation + str(secondNumber) + " = " + str(float(opAnswer))) # Print the answer to console
-            fileCreator(firstNumber, secondNumber, operation, float(opAnswer)) # Write the answer to the file
-    
     elif operation == "*":
         opAnswer = mul_numbers(firstNumber, secondNumber)
-        if noDecimal(opAnswer) == True: # Check if the answer has a decimal part
-            print("\n" + str(firstNumber) + operation + str(secondNumber) + " = " + str(int(opAnswer))) # Print the answer to console
-            fileCreator(firstNumber, secondNumber, operation, float(opAnswer)) # Write the answer to the file
 
-        elif noDecimal(opAnswer) == False:
-            print("\n" + str(firstNumber) + operation + str(secondNumber) + " = " + str(float(opAnswer))) # Print the answer to console
-            fileCreator(firstNumber, secondNumber, operation, float(opAnswer)) # Write the answer to the file
+    if noDecimal(opAnswer) == True:
+        opAnswer = int(opAnswer)
+    else:
+        opAnswer = float(opAnswer)
+
+    # Main Program Logic using if statements
+    print (f"\n{firstNumber} {operation} {secondNumber} = {opAnswer}")
+    fileCreator(firstNumber, secondNumber, operation, opAnswer)
     
+    loopCount += 1 # Increment the loop counter   
+    userInput = False # To Restart The Loop    
 
-clear_console()
-print("Thank you for using the calculator.")
-fileLocation()
-input("Press enter to continue...")
+# Exiting Program Stuff (Print output to the user, if a file was written gives the user it's location, then exits.)
+clear_console() # Clearing Console           
+print("Thank you for using the calculator.") 
+if loopCount > 0:                      
+    fileLocation()                           
+    if input("\nWould you like to open the folder? (y/n): ").lower() == "y":
+        openFileLocation()
+        print("\nFile location opened. Exiting program...")
+        consoleSleep2()
+        exit()
+    else:
+        print("\nExiting program...")
+        consoleSleep2()
+        exit()
+else:
+    print("\nExiting program...")
+    consoleSleep2()
+    exit()
